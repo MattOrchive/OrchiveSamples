e836f67a04a342a464da1bd31fae6a9b64132ebc
Matthew Vasseur <matt@orchive.com>
2011-06-17 21:48:20 -0400

    Fixed Sample App

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0fb3f68dddf372fd5307a989886edfdfd88b9eee5124f944a4766294615df88949f1d314df7a8e839fd8bee5d421a5a76fcd3fc91807dd49ab0fcafe9d51e301'
